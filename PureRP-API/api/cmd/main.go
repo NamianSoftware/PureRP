4da347c7d23edc8d4d2bb48eaae311bc90d23611
if-not-skill <regday@gmail.com>
2023-07-19 12:36:04 +0300

    [api] using viper for reading config

--- PureRP-API/api/cmd/main.go
+++ PureRP-API/api/cmd/main.go
@@ -5,16 +5,27 @@ import (
 	"api/pkg/handler"
 	"api/pkg/repository"
 	"api/pkg/service"
+	"github.com/spf13/viper"
 	"log"
 )
 
 func main() {
+	if err := initConfig(); err != nil {
+		log.Fatalf("error initializing config: %s", err.Error())
+	}
+
 	repos := repository.NewRepository()
 	services := service.NewService(repos)
 	handlers := handler.NewHandler(services)
 
 	srv := new(api.Server)
-	if err := srv.Run("8080", handlers.InitRoutes()); err != nil {
+	if err := srv.Run(viper.GetString("port"), handlers.InitRoutes()); err != nil {
 		log.Fatalf("error occured while running http server: %s", err.Error())
 	}
+}
+
+func initConfig() error {
+	viper.AddConfigPath("configs")
+	viper.SetConfigName("config")
+	return viper.ReadInConfig()
 }
